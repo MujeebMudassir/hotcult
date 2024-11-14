b468ba6327db5a2f61cfe71810685473238ac378
MujeebMudassir <mujeebmujeeb2071@gmail.com>
2024-11-14 12:26:25 +0530

    added route

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,14 @@ import Home from './pages/Home';
 
 const About = () => <div>About</div>;
 const Contact = () => <div>Contact</div>;
+// Opting into the future v7 behavior
+
 
 const App = () => {
   return (
-    <Router>
+    <Router >
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/hotcult" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
